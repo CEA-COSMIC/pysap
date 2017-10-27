0bddf797f8908d1cb5007b216ae5c22d34e8097e
Antoine Grigis <antoine.grigis@cea.fr>
2017-10-27 08:49:19 +0200

    sparse2d/python/cmake/Modules/BuildBoost: add boost dependency.

---
+++ sparse2d/python/cmake/Modules/BuildBoost.cmake
@@ -0,0 +1,39 @@
+#========================================================#
+# Build the Boost dependencies for the project using a specific version of python #
+#========================================================#
+
+set(BoostVersion 1.65.1)
+set(BoostSHA256 9807a5d16566c57fd74fb522764e0b134a8bbe6b6e8967b83afefd30dcd3be81)
+
+string(REGEX REPLACE "beta\\.([0-9])$" "beta\\1" BoostFolderName ${BoostVersion})
+string(REPLACE "." "_" BoostFolderName ${BoostFolderName})
+set(BoostFolderName boost_${BoostFolderName})
+
+ExternalProject_Add(Boost
+    PREFIX Boost
+    URL  http://sourceforge.net/projects/boost/files/boost/${BoostVersion}/${BoostFolderName}.tar.bz2/download
+    URL_HASH  SHA256=${BoostSHA256}
+    CONFIGURE_COMMAND ./bootstrap.sh
+                                                        --with-libraries=python
+                                                        --with-python=${PYTHON_EXECUTABLE}
+    BUILD_COMMAND           ./b2 install
+                                                        variant=release
+                                                        link=static
+                                                        cxxflags='-fPIC'
+                                                        --prefix=${CMAKE_BINARY_DIR}/extern
+                                                        -d 0
+                                                        -j8
+    INSTALL_COMMAND ""
+    BUILD_IN_SOURCE 1
+    )
+
+set(Boost_LIBRARY_DIR ${CMAKE_BINARY_DIR}/extern/lib/ )
+set(Boost_INCLUDE_DIR ${CMAKE_BINARY_DIR}/extern/include/ )
+
+if(${PYTHON_VERSION_STRING} GREATER 3.0)
+  message(STATUS "Using Python3")
+  set(Boost_LIBRARIES -lboost_python3 -lboost_numpy)
+else()
+  message(STATUS "Using Python2")
+  set(Boost_LIBRARIES -lboost_python -lboost_numpy)
+endif()
