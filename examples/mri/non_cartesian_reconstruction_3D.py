3bda7c539b7dba1de9b192ed2ce834ec6483faf7
Loubna EL GUEDDARI <loubna.el-gueddari@inria.fr>
2018-03-26 14:50:29 +0200

    [example]: Adding new non-cartesian example

--- examples/mri/non_cartesian_reconstruction_3D.py
+++ examples/mri/non_cartesian_reconstruction_3D.py
@@ -2,7 +2,7 @@
 Neuroimaging cartesian reconstruction
 =====================================
 
-Credit: A Grigis, L Elgueddari, H Carrie
+Credit: S Lannuzel, L Elgueddari
 
 In this tutorial we will reconstruct an MRI image from the sparse kspace
 measurments.
@@ -10,24 +10,28 @@ measurments.
 Import neuroimaging data
 ------------------------
 
-We use the toy datasets available in pISAP, more specifically a 2D brain slice
+We use the toy datasets available in pysap, more specifically a 3D brain slice
 and the acquistion cartesian scheme.
 We also add some gaussian noise in the image space.
 """
 
 # Package import
-from pisap.plugins.mri.reconstruct_3D.utils import imshow3D
-from pisap.plugins.mri.reconstruct_3D.utils import normalize_samples
-from pisap.plugins.mri.reconstruct.gradient import GradSynthesis2
-from pisap.plugins.mri.reconstruct.gradient import GradAnalysis2
-from pisap.plugins.mri.reconstruct_3D.linear import pyWavelet3
-from pisap.plugins.mri.reconstruct_3D.fourier import NFFT3
-from pisap.plugins.mri.reconstruct_3D.reconstruct import sparse_rec_fista
-from pisap.plugins.mri.reconstruct_3D.reconstruct import sparse_rec_condatvu
+from pysap.plugins.mri.reconstruct_3D.utils import imshow3D
+from pysap.plugins.mri.reconstruct_3D.utils import normalize_samples
+from pysap.plugins.mri.reconstruct_3D.utils import convert_locations_to_mask
+from pysap.plugins.mri.reconstruct_3D.utils import convert_mask_to_locations_3D
+from pysap.plugins.mri.parallel_mri.gradient import Grad2D_pMRI
+# from pysap.plugins.mri.reconstruct_3D.gradient import GradSynthesis3
+# from pysap.plugins.mri.reconstruct_3D.gradient import GradAnalysis3
+from pysap.plugins.mri.reconstruct_3D.linear import pyWavelet3
+from pysap.plugins.mri.reconstruct_3D.fourier import NFFT3
+from pysap.plugins.mri.parallel_mri.reconstruct import sparse_rec_fista
+from pysap.plugins.mri.parallel_mri.reconstruct import sparse_rec_condatvu
 
 
 # Third party import
 import numpy as np
+import scipy.fftpack as pfft
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
 
@@ -36,13 +40,14 @@ filename = '/neurospin/optimed/LoubnaElGueddari/p_MRI_CSGRE_3D/' \
             '2018-01-15_32ch_ref_nc1000_data/' \
             'meas_MID14_gre_800um_iso_128x128x128_FID24.mat'
 Iref = loadmat(filename)['ref']
-imshow3D(Iref, display=False)
 
+imshow3D(Iref, display=True)
 
-samples = loadmat('/neurospin/optimed/clazarus/Data/MRI_rawdata/CSGRE_3D/'
-                  '2018-01-15_32ch_ref_nc1000_ddata/'
-                  'samples_N256_nc1000_ns3072_ddata.mat')['samples']
+samples = loadmat('/volatile/data/sampling_schemes/'
+                  'samples_sparkling_3D_N128_502x1536x8_FID4971.mat')['samples']
 samples = normalize_samples(samples)
+cartesian_samples = convert_locations_to_mask(samples, [128, 128, 128])
+imshow3D(cartesian_samples, display=True)
 
 #############################################################################
 # Generate the kspace
@@ -53,22 +58,23 @@ samples = normalize_samples(samples)
 # We then reconstruct the zero order solution.
 
 # Generate the subsampled kspace
-fourier_data = NFFT3(samples, [128, 128, 128])
-kspace_data = fourier_data.op(Iref)
+kspace_loc = convert_mask_to_locations_3D(cartesian_samples)
+fourier_op_gen = NFFT3(samples=kspace_loc, shape=Iref.shape)
+kspace_data = fourier_op_gen.op(Iref)
 
 # Zero order solution
-image_rec0 = fourier_data.adj_op(kspace_data)
-imshow3D(np.abs(image_rec0), display=False)
+image_rec0 = fourier_op_gen.adj_op(kspace_data)
+imshow3D(np.abs(image_rec0), display=True)
 
 max_iter = 10
 
 linear_op = pyWavelet3(wavelet_name="sym4",
                        nb_scale=4)
 
-fourier_op = NFFT3(samples=samples, shape=Iref.shape)
-gradient_op = GradSynthesis2(data=kspace_data,
-                             fourier_op=fourier_op,
-                             linear_op=linear_op)
+fourier_op = NFFT3(samples=kspace_loc, shape=Iref.shape)
+gradient_op = Grad2D_pMRI(data=kspace_data,
+                          fourier_op=fourier_op,
+                          linear_op=linear_op)
 
 x_final, transform, cost = sparse_rec_fista(
     gradient_op=gradient_op,
@@ -79,9 +85,10 @@ x_final, transform, cost = sparse_rec_fista(
     atol=1e-4,
     verbose=1,
     get_cost=True)
+imshow3D(np.abs(x_final), display=True)
 
-gradient_op_cd = GradAnalysis2(data=kspace_data,
-                               fourier_op=fourier_op)
+gradient_op_cd = Grad2D_pMRI(data=kspace_data,
+                             fourier_op=fourier_op)
 x_final, transform = sparse_rec_condatvu(
     gradient_op=gradient_op_cd,
     linear_op=linear_op,
@@ -92,7 +99,7 @@ x_final, transform = sparse_rec_condatvu(
     tau=None,
     sigma=None,
     relaxation_factor=1.0,
-    nb_of_reweights=2,
+    nb_of_reweights=0,
     max_nb_of_iter=max_iter,
     add_positivity=False,
     atol=1e-4,
