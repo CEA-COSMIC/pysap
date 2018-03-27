[FIX] change normalization in NFFT

pysap/plugins/mri/reconstruct/fourier.py
@@ -161,7 +161,7 @@ class NFFT2(FourierBase):
             masked Fourier transform of the input image.
         """
         self.plan.f_hat = img
-        return self.plan.trafo()
+        return (1.0 / np.sqrt(self.plan.M)) * self.plan.trafo()
 
     def adj_op(self, x):
         """ This method calculates inverse masked non-cartesian Fourier
@@ -178,4 +178,4 @@ class NFFT2(FourierBase):
             inverse 2D discrete Fourier transform of the input coefficients.
         """
         self.plan.f = x
-        return (1.0 / self.plan.M) * self.plan.adjoint()
+        return (1.0 / np.sqrt(self.plan.M)) * self.plan.adjoint()