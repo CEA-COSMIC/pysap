85c2bce94b8a869527e15a8ebda76670a7c6d14c
Antoine Grigis <antoine.grigis@cea.fr>
2018-07-04 14:07:12 +0200

    pysap/scripts/pysap_gridsearch: start fixing PR#46, need to fix scripts.

---
+++ pysap/numerics/gridsearch.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+##########################################################################
+# pySAP - Copyright (C) CEA, 2017 - 2018
+# Distributed under the terms of the CeCILL-B license, as published by
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+# for details.
+##########################################################################
+
+"""
+This module contains shortcuts to gridsearch functions.
+"""
+
+# Package import
+from pysap.plugins.mri.gridsearch.gridsearch import grid_search
+from pysap.plugins.mri.gridsearch.recon import sparse_rec_condatvu
+from pysap.plugins.mri.gridsearch.recon import sparse_rec_fista
--- pysap/plugins/mri/gridsearch/gridsearch.py
+++ pysap/plugins/mri/gridsearch/gridsearch.py
@@ -1,5 +1,5 @@
 ##########################################################################
-# XXX - Copyright (C) XXX, 2017
+# pySAP - Copyright (C) CEA, 2017 - 2018
 # Distributed under the terms of the CeCILL-B license, as published by
 # the CEA-CNRS-INRIA. Refer to the LICENSE file or to
 # http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
@@ -32,13 +32,13 @@ def _get_final_size(param_grid):
 
     Parameters:
     ----------
-    param_grid: dict or list of dictionaries,
+    param_grid: dict or list of dictionaries
         Dictionary with parameters names (string) as keys and lists of
         parameter settings to try as values.
 
     Return:
     -------
-    size: int,
+    size: int
         the number of bytes of the extended carthesian grid a parameters.
     """
     tmp = {}  # same pattern than param_grid but store the size
@@ -53,40 +53,40 @@ def _get_final_size(param_grid):
 def grid_search(func, param_grid, wrapper=None, n_jobs=1, verbose=0):
     """ Run `func` on the carthesian product of `param_grid`.
 
-        Parameters:
-        -----------
-        func: function,
-            The reconstruction function from whom to tune the hyperparameters.
-            `func` return should be handle by wrapper if it's not a
-            simple np.ndarray image.
-        param_grid: dict or list of dictionaries,
-            Dictionary with parameters names (string) as keys and lists of
-            parameter settings to try as values: the grids spanned by each
-            dictionary in the list are explored.
-        wrapper: function, (default: None)
-            Handle the call of func if some pre-process or post-process
-            should be done. `wrapper` has a specific API:
-            `wrapper(func, **kwargs)`
-        n_jobs: int (default: 1),
-            The maximum number of concurrently running jobs, such as the number
-            of Python worker processes when backend=multiprocessing or the
-            size of the thread-pool when backend=threading. If -1 all CPUs
-            are used. If 1 is given, no parallel computing code is used at all,
-            which is useful for debugging. For n_jobs below -1,
-            (n_cpus + 1 + n_jobs) are used. Thus for n_jobs = -2,
-            all CPUs but one are used.
-        verbose: int (default: 0),
-            The verbosity level: if non zero, progress messages are printed.
-            Above 50, the output is sent to stdout. The frequency of the
-            messages increases with the verbosity level. If it more than 10,
-            all iterations are reported.
+    Parameters:
+    -----------
+    func: function
+        The reconstruction function from whom to tune the hyperparameters.
+        `func` return should be handle by wrapper if it's not a
+        simple np.ndarray image.
+    param_grid: dict or list of dictionaries
+        Dictionary with parameters names (string) as keys and lists of
+        parameter settings to try as values: the grids spanned by each
+        dictionary in the list are explored.
+    wrapper: function, (default: None)
+        Handle the call of func if some pre-process or post-process
+        should be done. `wrapper` has a specific API:
+        `wrapper(func, **kwargs)`
+    n_jobs: int (default: 1)
+        The maximum number of concurrently running jobs, such as the number
+        of Python worker processes when backend=multiprocessing or the
+        size of the thread-pool when backend=threading. If -1 all CPUs
+        are used. If 1 is given, no parallel computing code is used at all,
+        which is useful for debugging. For n_jobs below -1,
+        (n_cpus + 1 + n_jobs) are used. Thus for n_jobs = -2,
+        all CPUs but one are used.
+    verbose: int (default: 0)
+        The verbosity level: if non zero, progress messages are printed.
+        Above 50, the output is sent to stdout. The frequency of the
+        messages increases with the verbosity level. If it more than 10,
+        all iterations are reported.
 
-        Results:
-        --------
-        list_kwargs: dict,
-            the list of the params used for each reconstruction.
-        res: list,
-            the list of result for each reconstruction.
+    Results:
+    --------
+    list_kwargs: dict
+        the list of the params used for each reconstruction.
+    res: list
+        the list of result for each reconstruction.
     """
     if wrapper is None:
         wrapper = _default_wrapper
