475ff24c8bd084efb18723cc63264974a1bf632f
Antoine Grigis <antoine.grigis@cea.fr>
2018-07-04 10:15:09 +0200

    pysap/data: fix data url.

--- pysap/data.py
+++ pysap/data.py
@@ -41,7 +41,7 @@ from pysap.base.exceptions import Exception
 # Global parameters
 SAMPLE_DATE_FILES = {
     "mri-babouin512": {
-        "url": ("ftp://ftp.cea.fr/../../../../pub/unati/nsap/pysap/datasets/"
+        "url": ("ftp://ftp.cea.fr/pub/unati/nsap/pysap/datasets/"
                 "Ref_babouin_NEX32.mat"),
         "md5sum": "b651f7ea2a3d4fc26c4d603bb7caeb95"
      },
