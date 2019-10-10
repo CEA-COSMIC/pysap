983e7f300c18dd067d0ffac5e6c0dd566cfbeb76
chaithyagr <chaithyagr@gmail.com>
2019-10-10 17:41:14 +0200

    Add MRI Examples

--- examples/mri/cartesian_reconstruction.py
+++ examples/mri/cartesian_reconstruction.py
@@ -2,137 +2,96 @@
 Neuroimaging cartesian reconstruction
 =====================================
 
-Credit: A Grigis, L Elgueddari, H Carrie
+Author: Chaithya G R
 
 In this tutorial we will reconstruct an MRI image from the sparse kspace
-measurments.
+measurements.
 
 Import neuroimaging data
 ------------------------
 
 We use the toy datasets available in pysap, more specifically a 2D brain slice
-and the acquistion cartesian scheme.
-We also add some gaussian noise in the image space.
+and the cartesian acquisition scheme.
 """
 
 # Package import
-import pysap
+from modopt.math.metrics import ssim
+from mri.numerics.fourier import FFT2
+from mri.numerics.reconstruct import sparse_rec_fista
+from mri.numerics.utils import generate_operators
+from mri.numerics.utils import convert_mask_to_locations
 from pysap.data import get_sample_data
-from pysap.plugins.mri.numerics.reconstruct import sparse_rec_fista
-from pysap.plugins.mri.numerics.reconstruct import sparse_rec_condatvu
-from pysap.plugins.mri.numerics.utils import generate_operators
-from pysap.plugins.mri.numerics.utils import convert_mask_to_locations
+import pysap
 
 # Third party import
 import numpy as np
-import scipy.fftpack as pfft
 
 # Loading input data
-image = get_sample_data("mri-slice-nifti")
-image.data += np.random.randn(*image.shape) * 20.
-mask = get_sample_data("mri-mask")
+image = get_sample_data('2d-mri')
+
+# Obtain K-Space Cartesian Mask
+mask = get_sample_data("cartesian-mri-mask")
+
+# View Input
 image.show()
 mask.show()
-
 
 #############################################################################
 # Generate the kspace
 # -------------------
 #
-# From the 2D brain slice and the acquistion mask, we generate the acquisition
-# measurments, the observed kspace.
-# We then reconstruct the zero order solution.
+# From the 2D brain slice and the acquisition mask, we retrospectively
+# undersample the k-space using a cartesian acquisition mask
+# We then reconstruct the zero order solution as a baseline
 
-
-# Generate the subsampled kspace
-kspace_mask = pfft.ifftshift(mask.data)
-kspace_data = pfft.fft2(image.data) * kspace_mask
 
 # Get the locations of the kspace samples
-kspace_loc = convert_mask_to_locations(kspace_mask)
+kspace_loc = convert_mask_to_locations(np.fft.fftshift(mask.data))
+# Generate the subsampled kspace
+fourier_op = FFT2(samples=kspace_loc, shape=image.shape)
+kspace_data = fourier_op.op(image)
 
 # Zero order solution
-image_rec0 = pysap.Image(data=pfft.ifft2(kspace_data), metadata=image.metadata)
+image_rec0 = pysap.Image(data=fourier_op.adj_op(kspace_data),
+                         metadata=image.metadata)
 image_rec0.show()
 
+# Calculate SSIM
+base_ssim = ssim(image_rec0, image)
+print(base_ssim)
 
 #############################################################################
 # FISTA optimization
 # ------------------
 #
 # We now want to refine the zero order solution using a FISTA optimization.
-# Here no cost function is set, and the optimization will reach the
-# maximum number of iterations. Fill free to play with this parameter.
+# The cost function is set to Proximity Cost + Gradient Cost
 
 # Generate operators
 gradient_op, linear_op, prox_op, cost_op = generate_operators(
     data=kspace_data,
-    wavelet_name="BsplineWaveletTransformATrousAlgorithm",
+    wavelet_name="sym8",
     samples=kspace_loc,
     nb_scales=4,
+    mu=2 * 1e-7,
     non_cartesian=False,
     uniform_data_shape=None,
     gradient_space="synthesis",
-    padding_mode="symmetric")
-
-# linear_op.transform.info()
-# exit()
+    padding_mode="periodization")
 
 # Start the FISTA reconstruction
-max_iter = 20
-x_final, transform, costs, metrics = sparse_rec_fista(
-    gradient_op,
-    linear_op,
-    prox_op,
-    None,
-    mu=1e-9,
-    lambda_init=1.0,
-    max_nb_of_iter=max_iter,
-    atol=1e-4,
-    verbose=1)
-image_rec = pysap.Image(data=np.abs(x_final))
-image_rec.show()
-
-#############################################################################
-# Condata-Vu optimization
-# -----------------------
-#
-# We now want to refine the zero order solution using a Condata-Vu
-# optimization.
-# Here no cost function is set, and the optimization will reach the
-# maximum number of iterations. Fill free to play with this parameter.
-
-# Generate operators
-gradient_op, linear_op, prox_op, cost_op = generate_operators(
-    data=kspace_data,
-    wavelet_name="BsplineWaveletTransformATrousAlgorithm",
-    samples=kspace_loc,
-    nb_scales=4,
-    non_cartesian=False,
-    uniform_data_shape=None,
-    gradient_space="analysis",
-    padding_mode="symmetric")
-
-# Start the CONDAT-VU reconstruction
-max_iter = 20
-x_final, transform, costs, metrics = sparse_rec_condatvu(
+max_iter = 200
+x_final, costs, metrics = sparse_rec_fista(
     gradient_op,
     linear_op,
     prox_op,
     cost_op,
-    std_est=None,
-    std_est_method="dual",
-    std_thr=2.,
-    mu=1e-9,
-    tau=None,
-    sigma=None,
-    relaxation_factor=1.0,
-    nb_of_reweights=2,
+    lambda_init=1,
     max_nb_of_iter=max_iter,
-    add_positivity=False,
     atol=1e-4,
     verbose=1)
 image_rec = pysap.Image(data=np.abs(x_final))
 image_rec.show()
-cost_rec = pysap.Image(data=costs)
-cost_rec.show()
+# Calculate SSIM
+recon_ssim = ssim(image_rec, image)
+print('The Reconstruction SSIM is : ' + str(recon_ssim))
