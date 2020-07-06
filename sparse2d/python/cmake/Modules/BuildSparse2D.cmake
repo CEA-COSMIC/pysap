ad59fc7c15bd99f1eebb9f958efe4fff425bc0ef
Chaithya G R <chaithyagr@gmail.com>
2020-07-06 15:42:17 +0200

    Fix install issues for scikit-learn and ensure the binaries are installed right (#139)
    * Update install details
    
    * Fix Typo
    
    * Change the include dir
    
    * Fix Typo in includes
    
    * Remove unwanted fix and also fix travis to prevent adding PATH variable
    
    Co-authored-by: chaithyagr <chaithyagr@gitlab.com>

--- sparse2d/python/cmake/Modules/BuildSparse2D.cmake
+++ sparse2d/python/cmake/Modules/BuildSparse2D.cmake
@@ -11,17 +11,16 @@ ExternalProject_Add(sparse2d
     # GIT_TAG master
     DEPENDS cfitsio
     CONFIGURE_COMMAND cmake ../sparse2d
-        -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/extern
+        -DCMAKE_INSTALL_PREFIX=${SPARSE2D_INSTALL_DIR}/..
         -DCFITSIO_INCLUDE_DIRS=${cfitsio_INCLUDE_DIR}
         -DCFITSIO_LIBRARY_DIRS=${cfitsio_LIBRARY_DIR}
         -DCFITSIO_LIBRARIES=${cfitsio_LIBRARIES}
         -DCMAKE_BUILD_TYPE=RELEASE
     BUILD_COMMAND make install
         -j8
-    INSTALL_COMMAND ""
     BUILD_IN_SOURCE 0
     )
 
-set(sparse2d_LIBRARY_DIR ${CMAKE_BINARY_DIR}/extern/lib/ )
-set(sparse2d_INCLUDE_DIR ${CMAKE_BINARY_DIR}/extern/include/ )
+set(sparse2d_LIBRARY_DIR ${SPARSE2D_INSTALL_DIR}/../lib/ )
+set(sparse2d_INCLUDE_DIR ${SPARSE2D_INSTALL_DIR}/../include/ )
 set(sparse2d_LIBRARIES -lmga2d -lsparse3d -lsparse2d -lsparse1d -ltools)
